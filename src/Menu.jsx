104b46d604a9d8107371aa7788202ea330f53b76
Sulav <sulavshrestha023@gmail.com>
2022-05-19 22:15:24 +0545

    Menu added with images and decription

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -2,10 +2,17 @@ const Menu = ({ items }) => {
   return (
     <div className="section-center">
       {items.map((item) => {
-        const { id, img, title, category, desc, price } = item;
+        const { id, img, title, desc, price } = item;
         return (
           <article className="menu-item" key={id}>
-            <img src={img} alt={title} />
+            <img src={img} alt={title} className="photo" />
+            <div className="item-info">
+              <header>
+                <h3>{title}</h3>
+                <h4 className="price">${price}</h4>
+              </header>
+              <p className="i">{desc}</p>
+            </div>
           </article>
         );
       })}
